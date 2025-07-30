57991855ca1d3113b6cc80677bf002875bba021b
Igorromao94 <igor_romao94@hotmail.com>
2025-07-30 13:31:55 -0300

    alterações para as solicitações da cliente

--- src/JS/script.js
+++ src/JS/script.js
@@ -5,7 +5,53 @@ let coluna_3 = document.getElementById('colunas_3')
 let caixa_de_nav = document.getElementById('Caixa_de_navegacao')
 let link_menu = document.getElementsByClassName('link_alteravel')
 let linksArray = Array.from(link_menu);
+const radioButtons = document.querySelectorAll('input[name="radio_btn_slid"]');
+const btn_manual = document.getElementsByName('btn_manual');
+const slideLargura = window.innerWidth;
+const carrossel = document.getElementById('slides');
 
+
+carrossel.addEventListener('scroll', () => {
+    console.log('Scroll horizontal atual:', carrossel.scrollLeft);
+    switch (carrossel.scrollLeft) {
+        case 0:
+            document.querySelector('.slides').scrollTo({
+                left: 0,
+                behavior: 'smooth'
+            });
+            remover_marcacao()
+            btn_manual[0].classList.add('manual-btn-selected')
+            break;
+        case slideLargura*1:
+            document.querySelector('.slides').scrollTo({
+                left: slideLargura*1,
+                behavior: 'smooth'
+            });
+            remover_marcacao()
+            btn_manual[1].classList.add('manual-btn-selected')
+            break;
+        case slideLargura*2:
+            document.querySelector('.slides').scrollTo({
+                left: slideLargura*2,
+                behavior: 'smooth'
+            });
+            remover_marcacao()
+            btn_manual[2].classList.add('manual-btn-selected')
+            break;
+        case slideLargura*3:
+            document.querySelector('.slides').scrollTo({
+                left: slideLargura*3,
+                behavior: 'smooth'
+            });
+            remover_marcacao()
+            btn_manual[3].classList.add('manual-btn-selected')
+            break;
+
+        default:
+            console.log("Erro, esse indice é inexistente")
+            break;
+    }
+});
 
 function Abre_e_fecha_menu() {
     //Abrindo o menu
@@ -50,11 +96,13 @@ function Abre_e_fecha_menu() {
     }
 }
 
-const radioButtons = document.querySelectorAll('input[name="radio_btn_slid"]');
-const btn_manual = document.getElementsByName('btn_manual');
+function remover_marcacao() {
+    for (let i = 0; i < btn_manual.length; i++) {
+        btn_manual[i].classList.remove('manual-btn-selected')
+    }
+}
 
 function selecionar_slide(){
-
 // Adiciona o event listener a cada radio button
 radioButtons.forEach((radio, index) => {
     radio.addEventListener('change', function() {
@@ -63,8 +111,41 @@ radioButtons.forEach((radio, index) => {
                 btn_manual[i].classList.remove('manual-btn-selected')
             }
             btn_manual[index].classList.add('manual-btn-selected')
+            switch (index) {
+                case 0:
+                    document.querySelector('.slides').scrollTo({
+                        left: 0,
+                        behavior: 'smooth'
+                    });
+                    btn_manual[0].classList.add('manual-btn-selected')
+                    break;
+                case 1:
+                    document.querySelector('.slides').scrollTo({
+                        left: slideLargura*1,
+                        behavior: 'smooth'
+                    });
+                    btn_manual[1].classList.add('manual-btn-selected')
+                    break;
+                case 2:
+                    document.querySelector('.slides').scrollTo({
+                        left: slideLargura*2,
+                        behavior: 'smooth'
+                    });
+                    btn_manual[2].classList.add('manual-btn-selected')
+                    break;
+                case 3:
+                    document.querySelector('.slides').scrollTo({
+                        left: slideLargura*3,
+                        behavior: 'smooth'
+                    });
+                    btn_manual[3].classList.add('manual-btn-selected')
+                    break;
+
+                default:
+                    console.log("Erro, esse indice é inexistente")
+                    break;
+            }
         }
     });
 });
-
 }
